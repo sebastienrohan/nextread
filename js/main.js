d1d9b878e4eb2153893d3f1a37fbe7ee90ddbb1a
Sébastien Rohan <sebastien.rohan@openmailbox.org>
2016-01-26 19:04:08 +0100

    Deleting books now updates the sessionStorage object and the <ul> ids

--- js/main.js
+++ js/main.js
@@ -9,13 +9,22 @@
 	// load booklist from sessionStorage
 	var book_list_json = sessionStorage.getItem('booklist');
 	var book_list = JSON.parse(book_list_json);
-
-	// load var count from sessionStorage
 	if (book_list != null) {
-		var count = book_list['count'];
+
+		// retrieve count
+		var count = sessionStorage.getItem('count');
+
+		// populate ul #list with stored booklist
+		var retrieved_list = '';
+		for (var key in book_list) {
+			retrieved_list += '<li id=\"' + key + '\">' + book_list[key] + '</li>';
+		}
+		$('#list').html(retrieved_list);
 	}
 	else {
-		count = 0;
+
+		// create intial vars
+		var count = -1;
 		var book_list = {};
 	}
 
@@ -35,8 +44,8 @@
 			$('#search').val('').focus();
 			
 			// sessionStorage
-			book_list['count'] = count;
-			book_list['book_' + count] = title;
+			sessionStorage.setItem('count', count);
+			book_list[count] = title;
 			var book_list_json = JSON.stringify(book_list);
 			sessionStorage.setItem('booklist',book_list_json);
 		}
@@ -58,13 +67,44 @@
 	// delete book
 	$(function() {
 		$('#list').on('dblclick', 'li', function(){
-			var id_a_suppr = $(this).attr('id');
+			var id_to_del = parseInt($(this).attr('id'));
 			$(this).fadeOut(function(){
+				
+				// update list order numbers in the HTML
+				$('li').each(function(index){
+					if (index > id_to_del) {
+						var new_id = index;
+						new_id--;
+						this.id = new_id;
+					}
+				});
+
 				$(this).remove();
+				count--;
 			});
+			
+			// delete from sessionStorage
+			var book_to_del = String(id_to_del);
+			delete book_list[book_to_del];
+
+			// update list order numbers in the stored object's properties
+			$.each(book_list, function(key, value){
+				if (key > id_to_del) {
+					var new_key = parseInt(key);
+					new_key--;
+					book_list[new_key] = book_list[key];
+					delete book_list[key];
+				}
+			});
+
+			// write to sessionStorage
+			var book_list_json = JSON.stringify(book_list);
+			sessionStorage.setItem('booklist',book_list_json);
+
+
 		});
 		$('#list').on('doubletap', 'li', function(){
-			var id_a_suppr = $(this).attr('id');
+			var id_to_del = $(this).attr('id');
 			$(this).fadeOut(function(){
 				$(this).remove();
 			});
