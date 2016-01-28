31e3a1fee44347076e3a1002f65feefc5ec61375
Sébastien Rohan <sebastien.rohan@openmailbox.org>
2016-01-28 18:31:17 +0100

    Prevented from adding an empty book and added double-tap delete on mobile

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 (function ($) {
 
-
-
-	// init
+	// INIT
 	$('#search').focus();
 	
 	// load booklist from sessionStorage
 	var book_list_json = sessionStorage.getItem('booklist');
 	var book_list = JSON.parse(book_list_json);
 	if (book_list != null) {
-
 		// retrieve count
 		var count = sessionStorage.getItem('count');
 
@@ -21,102 +18,15 @@
 		$('#list').html(retrieved_list);
 	}
 	else {
-
 		// create intial vars
 		var count = -1;
 		var book_list = {};
 	}
 
-	// add book
-	$(function() {
-
-		function add() {
-			count++;
-			var title = $('#search').val();
-			var book = $('<li id=\"' + count + '\">' + title + '</li>');
-			if (title.length > 40) {
-				book.addClass('small');
-			}
-			book.hide();
-			$('#list').append(book);
-			book.fadeIn();
-			$('#search').val('').focus();
-			
-			// sessionStorage
-			sessionStorage.setItem('count', count);
-			book_list[count] = title;
-			var book_list_json = JSON.stringify(book_list);
-			sessionStorage.setItem('booklist',book_list_json);
-		}
-
-		// press Enter
-		$('#search').keypress(function(e){
-      		if(e.which == 13) {
-       			add();
-				return false;
-      		}
-      	});
-
-      	// click the + button
-		$('#add').click(function(){
-			add();
-		});
-	});
-
-	// delete book
-	$(function() {
-		$('#list').on('dblclick', 'li', function(){
-			var id_to_del = parseInt($(this).attr('id'));
-			$(this).fadeOut(function(){
-				
-				// update list order numbers in the HTML
-				$('li').each(function(index){
-					if (index > id_to_del) {
-						var new_id = index;
-						new_id--;
-						this.id = new_id;
-					}
-				});
-
-				$(this).remove();
-				count--;
-				sessionStorage.setItem('count', count);
-			});
-			
-			// delete from sessionStorage
-			var book_to_del = String(id_to_del);
-			delete book_list[book_to_del];
-
-			// update list order numbers in the stored object's properties
-			$.each(book_list, function(key, value){
-				if (key > id_to_del) {
-					var new_key = parseInt(key);
-					new_key--;
-					book_list[new_key] = book_list[key];
-					delete book_list[key];
-				}
-			});
-
-			// write to sessionStorage
-			var book_list_json = JSON.stringify(book_list);
-			sessionStorage.setItem('booklist',book_list_json);
-			sessionStorage.setItem('count', count);
-		});
-
-		// mobile & tablet
-		$('#list').on('doubletap', 'li', function(){
-			var id_to_del = $(this).attr('id');
-			$(this).fadeOut(function(){
-				$(this).remove();
-			});
-		});
-	});
-
-	// move book
+	// listen for book movement
 	$('ul').sortable({
-		cursor: "move",
-		stop: function(event, ui){
-			
+		cursor: 'move',
+		stop: function(event, ui){	
 	        // update list order numbers in the HTML & sessionStorage
 	        var book_nbr = $('li').length;
 	        $('li').each(function(index){
@@ -130,5 +40,99 @@
 			sessionStorage.setItem('booklist',book_list_json);
         }
 	});
+
+	// listen for deleteBook()
+	$('#list').on('dblclick', 'li', function(){
+		var self = this;
+		deleteBook(self);
+	});
+	// listen for deleteBook() on mobile
+	$('#list').on('doubletap', 'li', function(){
+		var self = this;
+		deleteBook(self);
+	});
+
+
+	// ADD BOOK
+	$(function() {
+
+		function add() {
+			var title = $('#search').val();
+			if (title.length != 0) {
+				count++;
+				var book = $('<li id=\"' + count + '\">' + title + '</li>');
+				if (title.length > 40) {
+					book.addClass('small');
+				}
+				book.hide();
+				$('#list').append(book);
+				book.fadeIn();
+				$('#search').val('').focus();
+				
+				// sessionStorage
+				sessionStorage.setItem('count', count);
+				book_list[count] = title;
+				var book_list_json = JSON.stringify(book_list);
+				sessionStorage.setItem('booklist',book_list_json);
+			}
+			else {
+				$('#search').focus();
+				return false;
+			}
+		}
+
+		// press Enter
+		$('#search').keypress(function(e){
+      		if(e.which == 13) {
+       			add();
+				e.preventDefault();
+      		}
+      	});
+
+      	// click the + button
+		$('#add').click(function(){
+			add();
+		});
+	});
+
+
+	// DELETE BOOK
+	function deleteBook(book_suppr) {
+		var id_to_del = parseInt($(book_suppr).attr('id'));
+		$(book_suppr).fadeOut(function(){
+			
+			// update list order numbers in the HTML
+			$('li').each(function(index){
+				if (index > id_to_del) {
+					var new_id = index;
+					new_id--;
+					this.id = new_id;
+				}
+			});
+
+			$(book_suppr).remove();
+			count--;
+			sessionStorage.setItem('count', count);
+		});
+		
+		// delete from sessionStorage
+		var book_to_del = String(id_to_del);
+		delete book_list[book_to_del];
+
+		// update list order numbers in the stored object's properties
+		$.each(book_list, function(key, value){
+			if (key > id_to_del) {
+				var new_key = parseInt(key);
+				new_key--;
+				book_list[new_key] = book_list[key];
+				delete book_list[key];
+			}
+		});
+
+		// write to sessionStorage
+		var book_list_json = JSON.stringify(book_list);
+		sessionStorage.setItem('booklist',book_list_json);
+		sessionStorage.setItem('count', count);
+	}
 
 })(jQuery);
