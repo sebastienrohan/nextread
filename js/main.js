5d322209a4402f8126653c50653c31bfc4111c4e
Sébastien Rohan <sebastien.rohan@openmailbox.org>
2016-02-04 16:33:40 +0100

    Added management of unknown books and correct saving of info after moving books with imported info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-(function ($) {
-
-
+(function ($){
 
 	// INIT
 	$('#search').focus();
@@ -8,14 +6,23 @@
 	// load booklist from sessionStorage
 	var book_list_json = sessionStorage.getItem('booklist');
 	var book_list = JSON.parse(book_list_json);
-	if (book_list != null) {
+	if (book_list != null){
 		// retrieve count
 		var count = sessionStorage.getItem('count');
 
 		// populate ul #list with stored booklist
 		var retrieved_list = '';
-		for (var key in book_list) {
-			var regen_book = '<li id=\'' + count + '\'>' + book_list[key].title + '<a href=\'' + book_list[key].link  + '\' target=\'_blank\'><div class=\'pull-right\'>' + book_list[key].rating + ' % <img src=\'' + book_list[key].icon + '\'></div></a></li>';
+		for (var key in book_list){
+			if (book_list[key].link == undefined ){
+				book_list[key].link = '';
+			}
+			if (book_list[key].icon == undefined ){
+				book_list[key].icon = '';
+			}
+			if (book_list[key].a == undefined ){
+				book_list[key].a = '';
+			}
+			var regen_book = '<li id=\'' + count + '\'>' + book_list[key].title + book_list[key].link + '<div class=\'pull-right\'>' + book_list[key].rating + ' % ' + book_list[key].icon + '</div>' + book_list[key].a + '</li>';
 			retrieved_list += regen_book;
 		}
 		$('#list').html(retrieved_list);
@@ -42,10 +49,27 @@
 	        	var book_li = this;
 	        	book_title = $(book_li).textOnly();
 	        	book_rating = parseInt($(book_li).children().text());
-	        	book_link = $('a', book_li).attr('href');
-	        	book_icon = $('img', book_li).attr('src');
+	        	if (isNaN(book_rating)) {
+	        		book_rating = '?';
+	        	}
+	        	if ($('a', book_li).attr('href') == undefined){
+	        		book_link = '';
+	        		book_a = '';
+	        	}
+	        	else {
+	        		book_link = $('a', book_li).attr('href');
+	        		book_link = '<a href=\'' + book_link  + '\' target=\'_blank\'>';
+	        		book_a = '</a>';
+	        	}
+	        	if ($('img', book_li).attr('src') == undefined){
+	        		book_icon = '';
+	        	}
+	        	else {
+	        		book_icon = $('img', book_li).attr('src');
+	        		book_icon = '<img src=\'' + book_icon + '\'>';
+	        	}
 
-	        	if (index < book_nbr) {
+	        	if (index < book_nbr){
 	        		// HTML
 	        		this.id = index;
 	        		// sessionStorage
@@ -53,6 +77,7 @@
 	        		book_list[index].rating = book_rating;
 	        		book_list[index].link = book_link;
 	        		book_list[index].icon = book_icon;
+	        		book_list[index].a = book_a;
 	        	}
 	        });
 	        var book_list_json = JSON.stringify(book_list);
@@ -73,15 +98,31 @@
 
 
 	// ADD BOOK
-	$(function() {
+	$(function(){
 
 		function add(){
 			var api_key = '6ad6214b9afeca197fbea7b1d6d52758b463689f';
 			var title = $('#search').val();
+			// Change each first letter to uppercase
+
 
 			function addBook(link, rating, icon){
-				var book = $('<li id=\'' + count + '\'>' + title + '<a href=\'' + link  + '\' target=\'_blank\'><div class=\'pull-right\'>' + rating + ' % <img src=\'' + icon + '\'></div></a></li>');
-				if (title.length > 40) {
+				if (icon == undefined){
+					icon = ' ';
+				}
+				else {
+					icon = '<img src=\'' + icon + '\'>';
+				}
+				if (link == undefined){
+					link = '';
+					a = '';
+				}
+				else {
+					link = '<a href=\'' + link  + '\' target=\'_blank\'>';
+					a = '</a>';
+				}
+				var book = $('<li id=\'' + count + '\'>' + title + link + '<div class=\'pull-right\'>' + rating + ' % ' + icon + '</div>' + a + '</li>');
+				if (title.length > 40){
 					book.addClass('small');
 				}
 				book.hide();
@@ -95,13 +136,14 @@
 				new_book.title = title;
 				new_book.rating = rating;
 				new_book.link = link;
+				new_book.a = a;
 				new_book.icon = icon;
 				book_list[count] = new_book;
 				var book_list_json = JSON.stringify(book_list);
 				sessionStorage.setItem('booklist',book_list_json);
 			}
 
-			if (title.length != 0) {
+			if (title.length != 0){
 				count++;
 				
 				// API call to idreambooks
@@ -109,7 +151,7 @@
 					url: 'http://idreambooks.com/api/books/reviews.json?q=' + title + '&key=' + api_key,
 					dataType: 'json',
 					success: function(json){
-						if (json.book.rating != undefined) {
+						if (json.book.rating != undefined){
 							var icon = json.book.to_read_or_not;
 							var link = json.book.detail_link;
 							var rating = json.book.rating;
@@ -133,7 +175,7 @@
 
 		// press Enter
 		$('#search').keypress(function(e){
-      		if(e.which == 13) {
+      		if(e.which == 13){
        			add();
 				e.preventDefault();
       		}
@@ -147,13 +189,13 @@
 
 
 	// DELETE BOOK
-	function deleteBook(book_suppr) {
+	function deleteBook(book_suppr){
 		var id_to_del = parseInt($(book_suppr).attr('id'));
 		$(book_suppr).fadeOut(function(){
 			
 			// update list order numbers in the HTML
 			$('li').each(function(index){
-				if (index > id_to_del) {
+				if (index > id_to_del){
 					var new_id = index;
 					new_id--;
 					this.id = new_id;
@@ -171,7 +213,7 @@
 
 		// update list order numbers in the stored object's properties
 		$.each(book_list, function(key, value){
-			if (key > id_to_del) {
+			if (key > id_to_del){
 				var new_key = parseInt(key);
 				new_key--;
 				book_list[new_key] = book_list[key];
